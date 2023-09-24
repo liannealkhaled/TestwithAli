c3c940c034e900a72b2f0d165710e27c13ad68f6
lianne <liannealq@hotmail.com>
2023-09-24 13:15:27 +0300

    24/9

---
+++ semishop.js
@@ -0,0 +1,9 @@
+async function clinic (){
+console.log (`WELCOME`)
+const name = await takeInput (`enter your name`);
+const age = await takeInput(`enter your age `);
+const q1 = await takeInput(`Any history of cardiovascular events?`)
+console.log (`This is a ${name}, a ${age}`)
+}
+
+clinic()
